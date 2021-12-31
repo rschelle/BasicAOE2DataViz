ed461d5f5dac3335a60329359b2a9d70139b17e5
rschelle <40246169+rschelle@users.noreply.github.com>
2021-12-31 13:57:30 -0500

    Update simple.py

--- simple.py
+++ simple.py
@@ -29,7 +29,8 @@ def createplot1(total_games_by_civ,streamlit_width):
     """
     games_by_civ_df = pd.DataFrame(total_games_by_civ)
     games_by_civ_df["civ"] = games_by_civ_df.index
-    top10df = games_by_civ_df.loc[ games_by_civ_df["win"] > 130000] ##this is the top 10. I picked 130000 manually
+    games_by_civ_df.sort_values(by = "win")
+    top10df = games_by_civ_df.loc[0:10] ##this is the top 10. I picked 130000 manually
     return px.bar(top10df, y = "win", labels = {"win": "Total Games Played"}, title = "Number of Games played by the 10 Most Popular Civs", width = 250*streamlit_width, height = 250*streamlit_width)
 
 def createplot2(total_games_by_civ,streamlit_width):
